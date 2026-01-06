2c01cd67033c281e609469752fda17f111c11dae
Andre VonderHeyde <andresheyde@gmail.com>
2026-01-06 18:01:17 -0500

    Creating yToTime function

--- features/calendar/layout/calendarLayout.ts
+++ features/calendar/layout/calendarLayout.ts
@@ -7,6 +7,21 @@ export const TIME_GUTTER_HEIGHT = 25;
 
 export const HOURS = 24;
 
+export type Time = {
+    hour: number,
+    minute: number
+}
+
 export function timeToY(hour: number, minute: number = 0, hourHeight = DEFAULT_HOUR_HEIGHT) {
     return hour * hourHeight + (minute/60) * hourHeight
+}
+
+export function yToTime(y: number, hourHeight = DEFAULT_HOUR_HEIGHT): Time {
+    if (hourHeight === 0) {
+        throw new Error(`Invalid hourHeight: ${hourHeight}`)
+    }
+    return {
+        hour: y/hourHeight,
+        minute: 60*(y%hourHeight)/hourHeight,
+    }
 }
